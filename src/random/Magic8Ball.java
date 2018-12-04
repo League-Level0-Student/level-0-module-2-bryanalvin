351d5e47bdf2009def1b69aab2cea234f4427f07
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-03 17:37:15 -0800

    Magic 8 ball

--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -8,11 +8,16 @@ import java.util.Random;
 
 public class Magic8Ball {
 
-	// 1. Make a main method that includes all the steps below….
-
+	// 1. Make a main method that includes all the steps below.
+	public static void main(String[] args) {
+		
+	
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+		int randomChoice = 0;
+		Random Keshin= new Random ();
+		randomChoice= Keshin.nextInt(6);
 	// 3. Print out this variable
+		System.out.println(randomChoice);
 
 	// 4. Get the user to enter a question for the 8 ball
 
@@ -31,5 +36,7 @@ public class Magic8Ball {
 	// 8. If the random number is 3
 
 	// -- write your own answer
+		
+	}
 
 }
