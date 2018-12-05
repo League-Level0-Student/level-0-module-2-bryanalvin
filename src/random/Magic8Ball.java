650e281bcb4535abda855425bdb955bf21f12ba2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-04 17:43:49 -0800

    I did magic 8 ball and validation

--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -1,42 +1,58 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2018
 //    Level 0
 
-
 package random;
 
 import java.util.Random;
+
+import javax.swing.JOptionPane;
 
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below.
 	public static void main(String[] args) {
-		
-	
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+
+		// 2. Make a variable that will hold a random number and put a random number
+		// into this variable using "new Random().nextInt(4)"
 		int randomChoice = 0;
-		Random Keshin= new Random ();
-		randomChoice= Keshin.nextInt(6);
-	// 3. Print out this variable
+		Random Keshin = new Random();
+		randomChoice = Keshin.nextInt(6);
+		// 3. Print out this variable
 		System.out.println(randomChoice);
 
-	// 4. Get the user to enter a question for the 8 ball
+		// 4. Get the user to enter a question for the 8 ball
+		JOptionPane.showMessageDialog(null, "Who is the best?");
+		// 5. If the random number is 0
 
-	// 5. If the random number is 0
+		// -- tell the user "Yes"
+		if (randomChoice == 0) {
+			JOptionPane.showMessageDialog(null, "Hayato-Kun");
+		}
+		// 6. If the random number is 1
 
-	// -- tell the user "Yes"
+		// -- tell the user "No"
+		if (randomChoice == 1) {
+			JOptionPane.showMessageDialog(null, "Chiharu-Chan");
+		}
+		// 7. If the random number is 2
 
-	// 6. If the random number is 1
+		// -- tell the user "Maybe you should ask Google?"
+		if (randomChoice == 2) {
 
-	// -- tell the user "No"
+			JOptionPane.showMessageDialog(null, "Saki-San");
+		}
+		// 8. If the random number is 3
 
-	// 7. If the random number is 2
+		// -- write your own answer
+		if (randomChoice == 3) {
+			JOptionPane.showMessageDialog(null, "Katashi-Sensei");
+		}
 
-	// -- tell the user "Maybe you should ask Google?"
-
-	// 8. If the random number is 3
-
-	// -- write your own answer
-		
+		if (randomChoice == 4) {
+			JOptionPane.showMessageDialog(null,"Makame-Chan");
+		}
+		if (randomChoice == 5) {
+			JOptionPane.showMessageDialog(null,"Satomi-Chan");
+		}
 	}
-
 }
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -9,17 +9,36 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
+
 		Random randomMaker = new Random();
-		
+
 		int randomNumber = randomMaker.nextInt(5);
-		
+
 		System.out.println(randomNumber);
 
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "Your breath smells nice");
+		}
+		if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "You are a lifesaver");
+		}
+		if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "You are good at overwatch");
+
+		}
+
+		if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "You will find the girl/boy of your dreams");
+
+		}
+		if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "Every girl/boy in school will like you");
+
+		}
 		// 1. Use each value of randomNumber to give the user a random compliment.
 
 		// 2. Repeat all the code above 10 times
-		
+
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
