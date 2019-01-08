46a60c70e3798f6a3422c18e70c942b77e41167f
ilabuser <ilabuser@172.16.116.81>
2019-01-07 17:51:29 -0800

    Note use the sad or happy and then change it to another song if the angry does not work then use the other ones

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -12,23 +12,32 @@ public class SoundtrackToLife {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
 		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
-		
+				JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Im in an Anime happy mood", "Deppresed", "Angry" }, null);
+
 		// 2. Their answer is stored in the userMood variable. Print it out.
+		System.out.println(userMood);
+		// 3. If they are in a stressed mood, use the playVideo method to play a calming
+		// song from YouTube.
+		if (userMood == 0) {
+			playVideo("http://bit.ly/video-for-happy");
+		}
+		if (userMood == 1) {
+			playVideo("http://bit.ly/video-for-sad");
 
-		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
+		}
+		if (userMood == 2) {
+			playVideo(" http://bit.ly/video-for-angry");
 
-		
+		}                                             
 		// 4. Play different songs for other moods.
 
-/**
-* If you can’t think of any, here are some you can use...
-*	http://bit.ly/video-for-happy
-*	http://bit.ly/video-for-sad
-*	http://bit.ly/video-for-angry
-**/
-		
+		/**
+		 * If you can’t think of any, here are some you can use...
+		 * http://bit.ly/video-for-happy http://bit.ly/video-for-sad
+		 * http://bit.ly/video-for-angry
+		 **/
+
 		// If you are seeing ads at the beginning of your videos, install Adblock.
 
 	}
@@ -42,6 +51,3 @@ public class SoundtrackToLife {
 		}
 	}
 }
-
-
-
